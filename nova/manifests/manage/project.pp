5516f3377c067a584fdadcaaffa339ed25bd9226
Adam Gandelman <adamg@canonical.com>
2011-06-05 22:44:59 -0700

    Set nova_manage providers as default for linux. Avoid explicitly setting providers during declaration.

--- nova/manifests/manage/project.pp
+++ nova/manifests/manage/project.pp
@@ -2,7 +2,6 @@ define nova::manage::project ( $owner ) {
   nova_project { $name:
     ensure => present,
     owner => $owner,
-    provider => 'nova_manage',
     notify => Exec["nova-db-sync"],
     require => [Class["nova::db"], Nova::Manage::Admin[$owner]],
   }
