a109a3a5305e59ae60a402c5a7bddef64f4cf2ed
Adam Gandelman <adamg@canonical.com>
2011-06-07 20:03:25 -0700

    nova_network - move parameter validation to the correct places.

--- nova/lib/puppet/type/nova_network.rb
+++ nova/lib/puppet/type/nova_network.rb
@@ -11,18 +11,16 @@ Puppet::Type.newtype(:nova_network) do
 
   newparam(:network) do
     desc "Network (ie, 192.168.1.0)"
+    newvalues(/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.0$/)
   end
 
   newparam(:available_ips) do
     desc "# of available IPs. Must be greater than 4."
+    validate do |value|
+      if value.to_i < 4
+        raise Puppet::Error, "ERROR - nova_network: Parameter available_ips must be an integer greater than 4."
+      end
+    end
   end
 
-  validate do
-    if !self[:network].match(/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.0$/) 
-      raise Puppet::Error, "ERROR - nova_network: Malformed network address #{self[:network]}. Parameter network must be passed as a valid network address (ie, 192.168.25.0)"
-    end
-    if self[:available_ips].to_i < 4
-      raise Puppet::Error, "ERROR - nova_network: Parameter available_ips must be an integer greater than 4."
-    end
-  end
 end
